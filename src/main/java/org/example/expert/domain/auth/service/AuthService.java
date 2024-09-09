90047056a73e952c3722b8995d250ccea4f2e19a
Changmin Kim <ksjchm4@gmail.com>
2024-09-09 11:33:33 +0900

    레벨 1-2: if-else delete

--- src/main/java/org/example/expert/domain/auth/service/AuthService.java
+++ src/main/java/org/example/expert/domain/auth/service/AuthService.java
@@ -35,7 +35,6 @@ public class AuthService {
 
         UserRole userRole = UserRole.of(signupRequest.getUserRole());
 
-
         User newUser = new User(
                 signupRequest.getEmail(),
                 encodedPassword,
